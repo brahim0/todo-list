4f059d30f425b35df75519ba6d04c7e3e3e8f668
brahim0 <brahimhm470@gmail.com>
2019-03-09 23:54:42 +0100

    rst commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+  render() {
+      const {item,handleChange,handleSubmit,editItem} =this.props;
+    return (
+      <div className="card card-body my-3">
+      <form onSubmit={handleSubmit}>
+       <div className="input-group">
+        <div className="input-group-prepend">
+        <div className="input-group-text bg-primary text-white">
+        <i className="fas fa-book"></i>
+        </div>
+        </div>
+        <input type="text" value={item} onChange={handleChange} className="form-control text-capitalize"  placeholder="Add todo item" />
+       <button type="submit" 
+       className={editItem? "btn btn-success btn-block mt-4 text-capitalize":"btn btn-primary btn-block mt-4 text-capitalize" }>{editItem ? "edit item": "add item"}</button>
+       </div>
+      </form>
+      </div>
+    )
+  }
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem';
+export default class TodoList extends Component {
+  render() {
+      const {items,clearList,handleDelete,handleEdit} = this.props;
+    return (
+      <ul className="list-group my-5">
+    <h3 className="text-capitalize text-center">Todo List</h3>
+    {items.map(
+        item=>{
+            return(
+                <TodoItem key={item.id} title={item.title}  id={item.id} handleDelete={()=>handleDelete(item.id)} handleEdit={()=>handleEdit(item.id)} />
+
+            )
+
+        }
+    )}
+    <button type="button" onClick={clearList} className="btn btn-danger btn-block mt-5 text-capitalize">Clear all list</button>
+      </ul>
+    )
+  }
+}
